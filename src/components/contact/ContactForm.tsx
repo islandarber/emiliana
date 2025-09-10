660a4161fb8e198dd15a3cc4211363194f155e56
Christina Vekri <christina.vek@gmail.com>
2025-09-10 13:42:14 +0200

    contant info and map

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -142,7 +142,7 @@ const ContactForm: React.FC = () => {
                   <div>
                     <p className="font-medium">Email</p>
                     <a href="mailto:info@emiliana-yoga.com" className="text-green-100 hover:text-white transition">
-                      info@emiliana-yoga.com
+                      tsoukala.emiliana@gmail.com
                     </a>
                   </div>
                 </div>
@@ -152,7 +152,7 @@ const ContactForm: React.FC = () => {
                   <div>
                     <p className="font-medium">Phone</p>
                     <a href="tel:+306912345678" className="text-green-100 hover:text-white transition">
-                      +30 691 234 5678
+                      +30 695 576 7949
                     </a>
                   </div>
                 </div>
@@ -162,8 +162,10 @@ const ContactForm: React.FC = () => {
                   <div>
                     <p className="font-medium">Studio Location</p>
                     <p className="text-green-100">
-                      123 Wellness Street<br />
-                      Athens, Greece 12345
+                      Spiti Psychotherapeías <br />
+                      Alexandrou Diákou 14, <br />
+                      Neó Irakleío 141 22, <br />
+                      Attica, Greece
                     </p>
                   </div>
                 </div>
@@ -171,8 +173,13 @@ const ContactForm: React.FC = () => {
               
               <div className="mt-8">
                 <p className="font-medium mb-2">Connect With Me</p>
-                <div className="flex space-x-4">
-                  <a href="#" className="text-white hover:text-green-200 transition">
+                <div className="flex space-x-6">
+                  <a 
+                  href="https://www.instagram.com/emiliana_ts" 
+                  className="text-white hover:text-green-200 transition"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  >
                     <svg 
                       xmlns="http://www.w3.org/2000/svg" 
                       className="h-5 w-5" 
@@ -182,7 +189,11 @@ const ContactForm: React.FC = () => {
                       <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
                     </svg>
                   </a>
-                  <a href="#" className="text-white hover:text-green-200 transition">
+                  <a href="https://www.facebook.com/emily.vinyasayoga" 
+                  className="text-white hover:text-green-200 transition"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  >
                     <svg 
                       xmlns="http://www.w3.org/2000/svg" 
                       className="h-5 w-5" 
@@ -190,16 +201,6 @@ const ContactForm: React.FC = () => {
                       viewBox="0 0 24 24"
                     >
                       <path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/>
-                    </svg>
-                  </a>
-                  <a href="#" className="text-white hover:text-green-200 transition">
-                    <svg 
-                      xmlns="http://www.w3.org/2000/svg" 
-                      className="h-5 w-5" 
-                      fill="currentColor" 
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
                     </svg>
                   </a>
                 </div>
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -17,19 +17,28 @@ const ContactPage: React.FC = () => {
             
             <div className="bg-gray-200 h-96 rounded-lg overflow-hidden relative">
               {/* This would be replaced with an actual map component */}
-              <div className="absolute inset-0 flex items-center justify-center">
-                <p className="text-gray-600 font-medium">
-                  Interactive Map Placeholder
-                </p>
+              <div className="bg-gray-200 h-96 rounded-lg overflow-hidden relative">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3132.497780296292!2d23.763901415631272!3d38.0533837797071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a1bd40b8f2e56f%3A0x3134c2da43a8173a!2sAlexandrou%20Diakou%2014%2C%20Nea%20Ionia%20141%2022%2C%20Greece!5e0!3m2!1sen!2sde!4v1694342238047!5m2!1sen!2sde"
+                  width="100%"
+                  height="100%"
+                  style={{ border: 0 }}
+                  allowFullScreen={true}
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
               </div>
             </div>
             
             <div className="mt-8 text-center">
               <p className="text-gray-700">
-                Studio Address: 123 Wellness Street, Athens, Greece 12345
+                Spiti Psychotherapeías <br />
+                Alexandrou Diákou 14, <br />
+                Neó Irakleío 141 22, <br />
+                Attica, Greece
               </p>
               <p className="text-gray-700 mt-2">
-                Nearest Public Transport: Syntagma Metro Station (10 min walk)
+                Nearest Public Transport: Irakleio Metro Station (Line M1 Green)
               </p>
             </div>
           </div>
